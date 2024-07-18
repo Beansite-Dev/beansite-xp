f1363e8ec2eb08c0f84bc74f5fd36436ba2ece72
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-07-17 23:05:36 -0400

    sum shit idfk i dont care if this gets cut off i think i chasnged somecscaling in the start menu oh my god i cant type

---
+++ src/beanpowered/assets/fs.js
@@ -0,0 +1,29 @@
+const FileSystem=[{
+    "name":"C",
+    "type":"rootdir",
+    "attribs":{
+        "lastModified":"7/17"
+    },
+    "children":[
+        // test file:
+        {
+            "name":"Hello.txt",
+            "type":"file",
+            "attribs":{
+                "lastModified":"7/17"
+            },
+            "children":`Hello, World!`
+        },
+        // test folder:
+        {
+            "name":"test_dir",
+            "type":"dir",
+            "attribs":{
+                "lastModified":"7/17"
+            },
+            "children":[
+
+            ]
+        },
+    ]
+}];
