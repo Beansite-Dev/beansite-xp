c677ebcaea93c7a61170c140505e3223bd7d0fcd
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-08-02 21:57:25 -0400

    added sentry

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,22 @@ import BeanXPRouter from './sdk/router.jsx';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RenderImage from './misc/renderImages.jsx';
 import FireBean from './firebean/firebean.jsx';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://d3d308c77b68a5b4df4a426854e52312@o4507710868684800.ingest.us.sentry.io/4507710903025664",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(<BeanXPRouter>
